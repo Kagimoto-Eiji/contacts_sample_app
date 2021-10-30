53a4f8a4b65c4e1400e162f6364784ca5fcde2f1
kagimotoeiji <thank.eiji1017@gmail.com>
2021-10-30 21:24:33 +0900

    開発環境で letter_opener を使用できるように設定

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -12,5 +12,7 @@ if Rails.env.production?
     domain: host,
     aythentication: 'plain'
   }
-
+elsif Rails.env.development?
+  ActionMailer::Base.default_url_options = { host: 'localhost:3000' }
+  ActionMailer::Base.delivery_method = :letter_opener_web
 end
