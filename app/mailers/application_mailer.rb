058de3e69fd83ade26fa29c76ea74be52b7cb2ec
kagimotoeiji <thank.eiji1017@gmail.com>
2021-10-30 20:38:23 +0900

    サンプルのメイラーを作成

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  # 送信元として表示するメールアドレスと表示名
+  default from: 'お問い合わせサンプルアプリ<rails.sample.1017@gmail.com>'
   layout 'mailer'
 end
