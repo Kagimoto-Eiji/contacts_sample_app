cb4958c4f3244b87f08940c1a5323abd66074868
kagimotoeiji <thank.eiji1017@gmail.com>
2021-10-30 20:25:52 +0900

    本番環境で Gmail を使用できるように設定

---
+++ config/initializers/mail.rb
@@ -0,0 +1,16 @@
+if Rails.env.production?
+  host = 'sheltered-hollows-76498.herokuapp.com'
+  # メール配信に失敗した場合にエラーを発生
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    port: 587,
+    adress: 'smtp.gmail.com',
+    user_name: Rails.application.credentials.gmail[:adress],
+    password: Rails.application.credentials.gmail[:password],
+    domain: host,
+    aythentication: 'plain'
+  }
+
+end
