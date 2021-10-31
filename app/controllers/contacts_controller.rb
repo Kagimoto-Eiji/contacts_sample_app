d2c62dd2b2522b3763130ed90a187fc31c2119e0
kagimotoeiji <thank.eiji1017@gmail.com>
2021-10-31 10:59:32 +0900

    お問い合わせフォームを実装

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,25 @@
+class ContactsController < ApplicationController
+  def index
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      # ユーザーに送信
+      ContactMailer.user_email(@contact).deliver_now
+      # 管理者に送信
+      ContactMailer.admin_email(@contact).deliver_now
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  # IPアドレスをパラメータに追加
+  def contact_params
+    params.require(:contact)
+          .permit(:name, :email, :contact)
+          .merge(remote_ip: request.remote_ip)
+  end
+end
